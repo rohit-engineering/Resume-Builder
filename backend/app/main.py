70550a62c116b46e1334ccf9fe8945900b8b3a6f
Rohit shrivastava <rk4817341gmail.com>
2026-07-08 00:12:26 +0530

    fix5

--- backend/app/main.py
+++ backend/app/main.py
@@ -41,6 +41,9 @@ app.add_middleware(
         "http://localhost:5173",
         "http://127.0.0.1:3000",
         "http://127.0.0.1:5173",
+
+        # Production frontend
+        "https://resume-builder-mini.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
